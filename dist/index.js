40bf4ffad080b1f9c03f0b9d1ac6de8e235b5b90
Alex McGill <alex.mcgill@outlook.com>
2024-05-25 00:53:02 +0100

    Add basic styling

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,18 @@
 const App = (root) => {
     if (!root)
         return;
+    // Styling
+    const { style } = root;
+    style.display = "flex";
+    style.flexDirection = "column";
+    style.width = "100%";
+    style.height = "100%";
+    style.backgroundColor = "white";
+    style.fontFamily = "Arial";
+    // Replacer
     const custom = root === null || root === void 0 ? void 0 : root.getElementsByTagName("CustomElement");
     for (const element of custom !== null && custom !== void 0 ? custom : []) {
         element.innerHTML = "<span>Custom Element</span>";
-        console.log(element);
     }
 };
 const root = document.getElementById("root");
--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 const App = (root: HTMLElement | null) => {
   if (!root) return;
 
+  // Styling
+  const { style } = root;
+  style.display = "flex";
+  style.flexDirection = "column";
+  style.width = "100%";
+  style.height = "100%";
+  style.backgroundColor = "white";
+  style.fontFamily = "Arial";
+
+  // Replacer
   const custom = root?.getElementsByTagName("CustomElement");
   for (const element of custom ?? []) {
     element.innerHTML = "<span>Custom Element</span>";
-    console.log(element);
   }
 };
 
