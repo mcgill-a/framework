e4c8ea54ff769bd5f15751ca2aa0c040087b933c
Alex McGill <alex.mcgill@outlook.com>
2024-05-25 00:41:23 +0100

    Add a prototype custom element into index.html

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
+const App = (root) => {
+    if (!root)
+        return;
+    const custom = root === null || root === void 0 ? void 0 : root.getElementsByTagName("CustomElement");
+    for (const element of custom !== null && custom !== void 0 ? custom : []) {
+        element.innerHTML = "<span>Custom Element</span>";
+        console.log(element);
+    }
+};
 const root = document.getElementById("root");
-console.log(root);
+App(root);
--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
+const App = (root: HTMLElement | null) => {
+  if (!root) return;
+
+  const custom = root?.getElementsByTagName("CustomElement");
+  for (const element of custom ?? []) {
+    element.innerHTML = "<span>Custom Element</span>";
+    console.log(element);
+  }
+};
+
 const root = document.getElementById("root");
-console.log(root);
+App(root);
