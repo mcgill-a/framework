f300140a8533fa3a7317dc63a088a9767e019c01
Alex McGill <alex.mcgill@outlook.com>
2024-05-25 01:53:46 +0100

    Added buttons

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
+const CustomElement: () => HTMLElement = () => {
+  let value = 0;
+
+  const getValue = () => value.toString();
+  const setValue = (x: number) => (value = x);
+
+  const button = document.createElement("button");
+  button.textContent = getValue();
+  button.style.display = "flex";
+  button.style.minWidth = "64px";
+  button.style.justifyContent = "center";
+  button.style.boxSizing = "border-box";
+  button.style.padding = "4px";
+
+  button.onclick = () => {
+    setValue(value + 1);
+    button.textContent = getValue();
+    console.log(value);
+  };
+  return button;
+};
+
 const App = (root: HTMLElement | null) => {
   if (!root) return;
 
@@ -9,11 +31,13 @@ const App = (root: HTMLElement | null) => {
   style.height = "100%";
   style.backgroundColor = "white";
   style.fontFamily = "Arial";
+  style.gap = "8px";
 
   // Replacer
   const custom = root?.getElementsByTagName("CustomElement");
+  console.log(custom);
   for (const element of custom ?? []) {
-    element.innerHTML = "<span>Custom Element</span>";
+    element.append(CustomElement());
   }
 };
 
